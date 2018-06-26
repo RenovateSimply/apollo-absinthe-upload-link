8bea97efa2d57d5337f5f389a203ae0f20363ba4
joergbaier <jorgbaier@gmail.com>
2018-06-26 17:10:16 -0500

    attempt 2

--- src/request.js
+++ src/request.js
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs/Observable'
+import { Subscriber } from 'rxjs/Subscriber'
 import 'rxjs/add/observable/dom/ajax'
 import 'rxjs/add/operator/map'
 
@@ -13,6 +14,22 @@ const request = opts =>
     body: opts.body,
     method: 'POST',
     headers: opts.headers,
+    progressSubscriber: Subscriber.create(
+      (progressEvent) => {
+        if (!progressEvent) return;
+        const { lengthComputable, loaded, total } = progressEvent;
+        if (lengthComputable) {
+          const detail = {
+            formData: opts.body,
+            progress: loaded / total,
+          };
+          const event = new CustomEvent('progressEvent', { detail });
+          window.dispatchEvent(event);
+        }
+      },
+      (error) => console.log(error),
+      () => console.log('complete'),
+    ),
   }).map(({ response }) => response)
 
 export default request
