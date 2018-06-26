46b50d0c250ee01f7d384bb9ca8de020a81f2bcc
joergbaier <jorgbaier@gmail.com>
2018-06-26 17:05:05 -0500

    revert to original

--- src/request.js
+++ src/request.js
@@ -1,36 +1,18 @@
 import { Observable } from 'rxjs/Observable'
-import { Subject } from 'rxjs/Subject'
 import 'rxjs/add/observable/dom/ajax'
 import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/merge'
 
 /**
  * Request function
  *
  * @param {Object} opts
  */
-const request = (opts) => {
-  const progressSubscriber = new Subject();
-  const obs = Observable.ajax({
+const request = opts =>
+  Observable.ajax({
     url: opts.uri,
     body: opts.body,
     method: 'POST',
     headers: opts.headers,
-    progressSubscriber,
-  });
-  progressSubscriber.merge(obs).subscribe((progressEvent) => {
-    if (!progressEvent) return;
-    const { lengthComputable, loaded, total } = progressEvent;
-    if (lengthComputable) {
-      const detail = {
-        formData: opts.body,
-        progress: loaded / total,
-      };
-      const event = new CustomEvent('progressEvent', { detail });
-      window.dispatchEvent(event);
-    }
-  });
-  return obs.map(({ response }) => response);
-};
+  }).map(({ response }) => response)
 
 export default request
